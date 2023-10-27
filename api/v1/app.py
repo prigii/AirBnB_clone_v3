c48691a083d8f960bda56f94e5825b340f2e012c
Peter Rigii <theonlyrigii@gmail.com>
2023-10-27 14:54:37 +0300

    error handler 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,6 +17,19 @@ def app_teardown(exception):
     """removes the current storage session after each request"""
     storage.close()
 
+#task 5
+# error handler for 404
+@app.errorhandler(404)
+def not_found(error):
+    """ Return json response for 404 not found message"""
+    response = ('error': 'Not found')
+    return jsonify(response), 404
+
 
 if __name__ == "__main__":
+    # get host and post from environment variables
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int (getenv('HBNB_API_PORT', 5000))
     app.run(0.0.0.0:5000)
+    # run app in threaded mode
+    app.run(host=HOST, port=PORT, threaded=True)
